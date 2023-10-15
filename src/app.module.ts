91e0caa494c0455dd9a17de04e47541fbce027e6
Grimlerih <kanatuulud@gmail.com>
2023-10-15 14:23:10 +0600

    feat: added main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { WishesModule } from './wishes/wishes.module';
+import { OffersModule } from './offers/offers.module';
+import { UsersModule } from './users/users.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
 
 @Module({
-  imports: [],
+  imports: [WishesModule, OffersModule, UsersModule, WishlistsModule],
   controllers: [AppController],
   providers: [],
 })
