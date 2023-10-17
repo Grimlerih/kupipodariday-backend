aee5846e7f7d40c8a74f5c52d836dd61b6eba060
Grimlerih <kanatuulud@gmail.com>
2023-10-17 18:26:20 +0600

    feat: add exception filters

---
+++ src/exceptions/server.exception.ts
@@ -0,0 +1,15 @@
+import { HttpException } from '@nestjs/common';
+import { ErrorCode, code2message, code2status } from './error-codes';
+
+export class ServerException extends HttpException {
+  constructor(code: ErrorCode) {
+    const message = code2message.get(code);
+    const status = code2status.get(code);
+
+    if (!message || !status) {
+      throw new Error(`Unknown error code: ${code}`);
+    }
+
+    super(message, status);
+  }
+}
