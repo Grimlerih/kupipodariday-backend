be4c8329a7511cae73609f3da9968e114fc7e368
Grimlerih <kanatuulud@gmail.com>
2023-10-18 22:46:57 +0600

    refactor: createUserDto

--- src/auth/dto/signin.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SignInDto {
-  @IsString()
-  password: string;
-
-  @IsString()
-  username: string;
-}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  @IsOptional()
+  about?: string;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+}
