86e048ea00ff9e580f43b778361a000b4f67246f
Grimlerih <kanatuulud@gmail.com>
2023-10-19 18:22:24 +0600

    feat: add userServices

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { FindUserDto } from './dto/find-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ServerException } from 'src/exceptions/server.exception';
+import { ErrorCode } from 'src/exceptions/error-codes';
+import { HashService } from 'src/hash/hash.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private hashService: HashService,
+  ) {}
+
+  async create(user: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where: { email: user.email },
+    });
+
+    if (existUser) throw new ServerException(ErrorCode.UserAlreadyExists);
+
+    const hashedPassword = await this.hashService.hashPassword(user.password);
+    user.password = hashedPassword;
+
+    return await this.userRepository.save(user);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // async findOne(query) {
+  //   const existUser = await this.userRepository.findOne({
+  //     where: [{ email: query }, { username: query }],
+  //   });
+  //   return existUser;
+  // }
+
+  async findId(id: number) {
+    const existUser = await this.userRepository.findOneBy({ id });
+    return existUser;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.userRepository.update(id, updateUserDto);
+    return this.findId(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getMyWishes(id: number) {
+    const user = await this.userRepository.findOne({
+      where: { id },
+      relations: {
+        wishes: { owner: true, offers: true },
+      },
+    });
+    return user.wishes;
+  }
+
+  async findUserName(name: string) {
+    const user = await this.findUser(name);
+    return user;
+  }
+
+  async findUserNameWishes(name: string) {
+    const userWishes = await this.userRepository.findOne({
+      where: { username: name },
+      relations: {
+        offers: { user: true, item: true },
+      },
+    });
+    return userWishes.offers;
+  }
+
+  async findUser(query: string) {
+    const existUser = await this.userRepository.findOne({
+      where: [{ email: query }, { username: query }],
+    });
+    return existUser;
   }
 }
