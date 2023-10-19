00e29a06b20a9125dbf5e86f982b127a0428ab1d
Grimlerih <kanatuulud@gmail.com>
2023-10-19 18:21:57 +0600

    feat: add user controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { ExcludePasswordInterceptor } from 'src/interceptor/user-password.interceptor';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { FindUserDto } from './dto/find-user.dto';
 
+@UseGuards(AuthGuard('jwt'))
+@UseInterceptors(ExcludePasswordInterceptor)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Get('me')
+  async findMe(@Request() { user: { id } }) {
+    return await this.usersService.findId(id);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @Patch('me')
+  async update(
+    @Request() { user: { id } },
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get('me/wishes')
+  async getMyWishes(@Request() { user: { id } }): Promise<Wish[]> {
+    return this.usersService.getMyWishes(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Get(':username')
+  async findUserByName(@Param('username') username: string) {
+    return this.usersService.findUserName(username);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get(':username/wishes')
+  async getUserWishes(@Param('username') username: string) {
+    return this.usersService.findUserNameWishes(username);
+  }
+
+  @Post('find')
+  async findUser(@Body() query: FindUserDto) {
+    return this.usersService.findUser(query.query);
   }
 }
