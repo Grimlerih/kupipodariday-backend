c7a21383488dc6484c55490ed280bafc651cf237
Grimlerih <kanatuulud@gmail.com>
2023-10-21 14:17:43 +0600

    feat: createOffer DTO

--- src/offers/dto/create-offer.dto.ts
+++ src/offers/dto/create-offer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateOfferDto {}
+import { IsBoolean, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateOfferDto {
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsBoolean()
+  hidden: boolean;
+
+  @IsNumber()
+  @IsNotEmpty()
+  itemId: number;
+}
